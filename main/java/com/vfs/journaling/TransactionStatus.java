6214681b16b4f6a33a265df73460a1ba2099dab6
Freya1221 <168391810+Freya1221@users.noreply.github.com>
2026-03-22 21:56:52 -0400

    subida por error

--- main/java/com/vfs/journaling/TransactionStatus.java
+++
@@ -1,5 +0,0 @@
-package com.vfs.journaling;
-
-public enum TransactionStatus {
-    PENDIENTE, CONFIRMADO, UNDO
-}
--- main/java/com/vfs/process/ProcessState.java
+++
@@ -1,5 +0,0 @@
-package com.vfs.process;
-
-public enum ProcessState {
-    NEW, READY, RUNNING, BLOCKED, TERMINATED
-}
