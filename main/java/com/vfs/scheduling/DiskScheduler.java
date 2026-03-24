b24c71d9db4a60d5d8067886f6a8606e25ac43c7
josegabrieldeviana <josegabrieldeviana@gmail.com>
2026-03-23 22:23:39 -0400

    commit final. añadidura de mejor vista de cabezal, parametros de simulacion y solicitudes. mejoras.

--- main/java/com/vfs/scheduling/DiskScheduler.java
+++
@@ -1,7 +0,0 @@
-package com.vfs.scheduling;
-
-import com.vfs.structures.CustomLinkedList;
-
-public interface DiskScheduler {
-    CustomLinkedList<Integer> schedule(int currentHead, CustomLinkedList<Integer> requests, int maxBlock);
-}
